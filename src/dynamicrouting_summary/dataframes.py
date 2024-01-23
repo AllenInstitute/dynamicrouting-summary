c12232d7060597ee5caecff8c9ccc8037976b4ee
bjhardcastle <ben.hardcastle@alleninstitute.org>
2024-01-23 12:34:33 -0800

    Change default arg for `get_dfs()`

--- src/dynamicrouting_summary/dataframes.py
+++ src/dynamicrouting_summary/dataframes.py
@@ -7,7 +7,7 @@ import pandas as pd
 import dynamicrouting_summary.utils as utils
 
 
-def get_dfs(version: str | None = None, with_bool_columns: bool = False) -> dict[str | npc_lims.NWBComponentStr, pd.DataFrame]:
+def get_dfs(version: str | None = None, with_bool_columns: bool = True) -> dict[str | npc_lims.NWBComponentStr, pd.DataFrame]:
     """Get a dictionary of dataframes for each table-like component in an NWB file (except units)."""
     components = (c for c in typing.get_args(npc_lims.NWBComponentStr) if c != "units")
     dfs = {
