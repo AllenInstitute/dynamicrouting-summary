7077f632b97e4a82d97c54279c6ed00452c6d120
bjhardcastle <ben.hardcastle@alleninstitute.org>
2024-01-24 11:44:59 -0800

    Add `is_opto`

--- src/dynamicrouting_summary/utils.py
+++ src/dynamicrouting_summary/utils.py
@@ -10,7 +10,7 @@ def get_session_bools_df(version: str | None = None) -> pd.DataFrame:
     
     >>> get_session_bools_df().columns
     Index(['session_id', 'is_ephys', 'is_templeton', 'is_training',
-           'is_dynamic_routing'],
+           'is_dynamic_routing', 'is_opto'],
           dtype='object')
     """
     session_df = pd.read_parquet(npc_lims.get_cache_path('session', version=version))
@@ -21,6 +21,7 @@ def get_session_bools_df(version: str | None = None) -> pd.DataFrame:
             is_templeton = (is_templeton := session_df.keywords.map({'Templeton'}.issubset)),
             is_training = session_df.keywords.map({'training'}.issubset),
             is_dynamic_routing = ~is_templeton,
+            is_opto = session_df.keywords.map({'opto'}.issubset),
         )
     )
     session_bools_df = pd.concat([session_df['session_id'].reset_index(drop=True), bools_df.reset_index(drop=True)], axis=1)
@@ -50,34 +51,11 @@ def add_bool_columns(df: pd.DataFrame, version: str | None = None) -> pd.DataFra
         subject_id        date  session_idx
     0       660023  2023-08-09            0
     >>> add_bool_columns(df)
-        subject_id        date  session_idx           session_id  is_ephys  is_templeton  is_training  is_dynamic_routing
-    0       660023  2023-08-09            0  660023_2023-08-09_0      True         False        False                True
+        subject_id        date  session_idx           session_id  is_ephys  is_templeton  is_training  is_dynamic_routing  is_opto
+    0       660023  2023-08-09            0  660023_2023-08-09_0      True         False        False                True    False
     """
     session_bools_df = get_session_bools_df(version=version)
     return add_session_id_column(df).merge(session_bools_df, on=['session_id'])  
-
-@functools.cache
-def get_ephys_session_ids() -> set[str]:
-    """Get a list of session_ids for ephys sessions."""
-    return set(info.id for info in npc_lims.get_session_info(is_ephys=True))
-
-@functools.cache
-def get_templeton_session_ids() -> set[str]:
-    """Get a list of session_ids for ephys sessions."""
-    return set(info.id for info in npc_lims.get_session_info(is_templeton=True))
-
-@functools.cache
-def get_bools(session_id: str) -> dict[str, bool]:
-    """Get a dict of bools for is_ephys, is_templeton, is_training,
-    is_dynamic_routing, etc. for adding as dataframe columns."""
-    assert len(session_id.split('_')) == 3, f"session_id {session_id} is not in the form of subject_id_date_session_idx"
-    return {
-        "is_ephys": (is_ephys := session_id in get_ephys_session_ids()),
-        "is_templeton": (is_templeton := session_id in get_templeton_session_ids()),
-        "is_training": not is_ephys,
-        "is_dynamic_routing": not is_templeton,
-    }
-
 
 if __name__ == '__main__':
     import doctest
